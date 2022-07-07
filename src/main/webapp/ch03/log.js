6c918a25bdb4df168abedb04c1e2687b3e2eaee9
wchori00 <swc198888@gmail.com>
2022-07-07 19:35:52 +0900

    ajax DOM 예제 실습 주석 추가

--- src/main/webapp/ch03/log.js
+++ src/main/webapp/ch03/log.js
@@ -1,6 +1,9 @@
 function log(msg) {
 	var console = document.getElementById("debugConsole");
-	if(console != null) {
+	// id값이 debugConsole에 해당하는 모든 Element 목록 console에 저장
+	if(console != null) {	// 만약 console이 null이 아니면
 		console.innerHTML += msg + "<br>";
+		// console에 innerHTML()메서드를 통해서 text문장 입력
+		// text문장: usingDOM.html의 log()값 + 줄바꿈 누적 입력
 	}
 }
